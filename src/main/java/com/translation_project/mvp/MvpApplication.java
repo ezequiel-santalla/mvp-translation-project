bc1c39089e4749c30358701f53cb5b4ca2c87014
Ezequiel Santalla <ezequielasantalla@gmail.com>
2024-11-26 20:46:50 -0300

    starting point of the app

---
+++ src/main/java/com/translation_project/mvp/MvpApplication.java
@@ -0,0 +1,13 @@
+package com.translation_project.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpApplication.class, args);
+	}
+
+}
