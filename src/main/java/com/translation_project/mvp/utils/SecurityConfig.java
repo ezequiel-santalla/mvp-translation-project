bbacb10d2d2c676ce43d55ac85ed9510471b9097
Ezequiel Santalla <ezequielasantalla@gmail.com>
2024-11-27 21:38:42 -0300

    SecurityConfig class created to enable test http requests

---
+++ src/main/java/com/translation_project/mvp/utils/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.translation_project.mvp.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/", "/swagger-ui/", "/swagger-ui.html", "/v3/api-docs*/", "/h2-console/").permitAll()
+                        .requestMatchers("/users").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        return http.build();
+    }
+}
